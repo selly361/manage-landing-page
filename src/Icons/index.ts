6878167396985a342472cc45448666245713abf8
seikou <seikou615@gmail.com>
2023-08-16 19:28:48 +0100

    Add: Exported Icons

---
+++ src/Icons/index.ts
@@ -0,0 +1,8 @@
+export { ReactComponent as CloseIcon } from "./icon-close.svg"
+export { ReactComponent as OpenIcon } from "./icon-hamburger.svg"
+export { ReactComponent as LogoIcon } from "./icon-logo.svg"
+export { ReactComponent as FacebookIcon } from "./icon-facebook.svg"
+export { ReactComponent as InstagramIcon } from "./icon-instagram.svg"
+export { ReactComponent as PinterestIcon } from "./icon-pinterest.svg"
+export { ReactComponent as TwitterIcon } from "./icon-twitter.svg"
+export { ReactComponent as YoutubeIcon } from "./icon-youtube.svg"
