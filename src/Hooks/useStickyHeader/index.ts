f3d28f1cc9979170cfec194fc7a8ac04dff25f60
seikou <seikou615@gmail.com>
2023-08-19 19:18:08 +0100

    Add: `useStickyHeader` hook

---
+++ src/Hooks/useStickyHeader/index.ts
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+
+const useStickyNav = () => {
+
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [ isIntersecting, setIsIntersecting ] = useState(false);
+    const [ isNotIntersecting, setIsNotIntersecting ] = useState(false);
+    const transitionDuration = 300;
+    let isInitialized = false;
+    let timer;
+
+    useEffect(() => {
+        if(containerRef.current) {
+            navObserver(containerRef.current);
+        }
+    }, [containerRef]);
+
+
+    const navObserver = (element: HTMLElement) => {
+        if (!element) return false;
+
+        const options = {
+            threshold: 0.3
+        }
+
+        const observer = new IntersectionObserver((entries) => {
+            if (isInitialized) {
+                entries.forEach(entry => {
+                    if (!entry.isIntersecting) {
+                        setIsIntersecting(true);
+                        setIsNotIntersecting(false);
+                    } else {
+                        setIsNotIntersecting(true);
+                        
+                        timer = setTimeout(() => {
+                            setIsIntersecting(false);
+                            setIsNotIntersecting(false);
+
+                            timer = null;
+                        }, transitionDuration)
+                    }
+                })
+            }
+            isInitialized = true;
+        }, options)
+        observer.observe(element);
+    }
+
+    return { containerRef, isScrollIn: isIntersecting, isScrollOut: isNotIntersecting }
+}
+ 
+export default useStickyNav;
