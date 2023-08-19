d5c4d7638082b5af6a83eb78565dc316e7673d48
seikou <seikou615@gmail.com>
2023-08-19 19:34:42 +0100

    Update: refactired with components and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { AboutSection, Header } from 'Layout'
+
+import { Fragment } from 'react'
+import { useNavMenuContext } from 'Context'
+import useStickyNav from 'Hooks/useStickyHeader'
 
 function App() {
-  return null
+  const { isNavOpen } = useNavMenuContext()
+
+  const { isScrollIn, isScrollOut, containerRef } = useStickyNav()
+
+
+  return (
+    <Fragment>
+      <Header isScrollIn={isScrollIn} isScrollOut={isScrollOut} />
+      {isNavOpen ? <div className="overlay" /> : null}
+      <main ref={containerRef} className="main">
+        <AboutSection />
+      </main>
+    </Fragment>
+  )
 }
 
 export default App
